c403e2e2b9286c9431785a4b81881d55bdcf1a41
MateuszSzybiak <matszybiak@gmail.com>
2022-03-06 17:26:22 +0100

    Reformat files

--- src/cleaning.py
+++ src/cleaning.py
@@ -49,18 +49,3 @@ def stemming(text: str) -> list:
     for word in list_of_words:
         result_list.append(porter.stem(word))
     return result_list
-
-
-example = r"   dadah DADVA 12 dada 13 :)  i ;<  daDua12 me daIDba <xp>dad12ad" \
-          r" </xp> dau.  ours;    daiusd,   you    13"
-# print(example)
-# print(cleaning_text(example))
-# print(remove_stop_words(cleaning_text(example)))
-
-an_ex = r"Hello my friend ;). What have you done? Are you crazy? 12 cows " \
-        r"are here and 7 pigs and you did nothing. Connection, connecting, connected. What a irony :)"
-
-print(an_ex)
-print(cleaning_text(an_ex))
-print(remove_stop_words(cleaning_text(an_ex)))
-print(stemming(remove_stop_words(cleaning_text(an_ex))))
