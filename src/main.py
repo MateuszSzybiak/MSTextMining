c17e0fdf92cdd5c924f2405b6d8c84e25553d584
MateuszSzybiak <matszybiak@gmail.com>
2022-03-13 09:44:40 +0100

    Clean file

--- src/main.py
+++ src/main.py
@@ -10,15 +10,6 @@ from wordcloud import WordCloud
 from tqdm import tqdm
 
 
-# an_ex = r"Hello my friend ;). What have you done? Are you crazy? 12 cows " \
-#         r"are here and 7 pigs and you did nothing. Connection, connecting, connected. What a irony :)"
-
-# print(an_ex)
-# print(cleaning_text(an_ex))
-# print(remove_stop_words(cleaning_text(an_ex)))
-# print(stemming(remove_stop_words(cleaning_text(an_ex))))
-
-
 df = pd.read_csv(r"D:\Pliki\Studia II stopnia\Semestr II\Text mining\News _dataset\True.csv")
 
 string = ""
